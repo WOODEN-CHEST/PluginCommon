86472a15ab10b99f3f950104591ec53d8ed4c7ea
amogus <86293484+WOODEN-CHEST@users.noreply.github.com>
2025-01-16 17:40:23 +0200

    added id peeking

--- src/sus/keiger/plugincommon/SequentialIDProvider.java
+++ src/sus/keiger/plugincommon/SequentialIDProvider.java
@@ -2,8 +2,24 @@ package sus.keiger.plugincommon;
 
 public class SequentialIDProvider implements IIDProvider
 {
+    // Static fields.
+    public static final long INVALID_ID = 0;
+
+
     // Private fields.
     private long _availableID = 1L;
+
+
+    // Methods.
+    public void SetCurrentID(long value)
+    {
+        _availableID = value;
+    }
+
+    public long PeekID()
+    {
+        return _availableID;
+    }
 
 
     // Inherited methods.
